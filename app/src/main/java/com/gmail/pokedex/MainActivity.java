0f6ee898d955c5b7c88f768cbac57a799d761d8c
glennkok <s10208391@connect.np.edu.sg>
2021-09-09 20:31:08 +0800

    removed starting network load

--- app/src/main/java/com/gmail/pokedex/MainActivity.java
+++ app/src/main/java/com/gmail/pokedex/MainActivity.java
@@ -6,6 +6,7 @@ import androidx.recyclerview.widget.DefaultItemAnimator;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
@@ -14,31 +15,44 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.ProgressBar;
+import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 import com.gmail.pokedex.Main.Adapters.PokemonAdapter;
 import com.gmail.pokedex.Model.Pokemon;
 import com.gmail.pokedex.Model.PokemonBrief;
-import com.gmail.pokedex.Network.PokeAPI;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
     private ArrayList<PokemonBrief> pokemonList = new ArrayList<>();
-    private PokeAPI pokeAPI;
+    private Context context;
     private ProgressBar progressBar;
-    private RecyclerView mainPokemonRV;
     private PokemonAdapter pokemonAdapter;
     private FloatingActionButton fab;
     private ConstraintLayout layout;
     private LinearLayout searchLayout;
+    private final String DATA_URL = "https://raw.githubusercontent.com/glennkok1501/SimpleDexAPI/main/data/all.json";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mainPokemonRV = findViewById(R.id.main_pokemon_RV);
+        context = this;
+        RequestQueue mQueue = Volley.newRequestQueue(context);
+
+        RecyclerView mainPokemonRV = findViewById(R.id.main_pokemon_RV);
         progressBar = findViewById(R.id.main_pokemon_progressbar);
         fab = findViewById(R.id.main_pokemon_fab);
         layout = findViewById(R.id.main_pokemon_layout);
@@ -51,28 +65,38 @@ public class MainActivity extends AppCompatActivity {
         pokemonAdapter = new PokemonAdapter(pokemonList, this);
         mainPokemonRV.setAdapter(pokemonAdapter);
 
-        pokeAPI = new PokeAPI(MainActivity.this);
-        pokeAPI.initPokemons(pokemonList);
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, DATA_URL, null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            progressBarStatus(false);
+                            JSONArray results = response.getJSONArray("pokemon");
+                            for (int i = 0; i < results.length(); i++){
+                                PokemonBrief p = parsePokemon(results.getJSONObject(i));
+                                if (p != null){
+                                    pokemonList.add(p);
+                                }
+                            }
+                            pokemonAdapter.updateData();
+                            progressBarStatus(true);
+                        }
 
-        new Thread() {
-            public void run() {
-                try{
-                    while (pokemonList.size() < 1038){
-                        progressBarStatus(false);
-//                        Log.v("TAG", "LIST SIZE - "+pokemonList.size());
-                        updateListThread(mainPokemonRV, pokemonAdapter);
-                        Thread.sleep(100);
+                        catch (Exception e) {
+                            Toast.makeText(context, "Data unavailable", Toast.LENGTH_SHORT).show();
+                            progressBarStatus(false);
+                            e.printStackTrace();
+                        }
                     }
-//                    Log.v("TAG", "LIST SIZE - FINISHED "+pokemonList.size());
-                    updateListThread(mainPokemonRV, pokemonAdapter);
-                    progressBarStatus(true);
-
-                }
-                catch (Exception e) {
-                    e.printStackTrace();
-                }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(context, "Data unavailable", Toast.LENGTH_LONG).show();
+                progressBarStatus(false);
+                error.printStackTrace();
             }
-        }.start();
+        });
+        mQueue.add(request);
 
         searchEditText.addTextChangedListener(new TextWatcher() {
             @Override
@@ -90,8 +114,6 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
-
-
     }
 
     private ArrayList<PokemonBrief> filter(String search_string){
@@ -132,23 +154,25 @@ public class MainActivity extends AppCompatActivity {
         }
         int finalProg_vis = prog_vis;
         int finalFab_vis = fab_vis;
-        layout.post(new Runnable() {
-            @Override
-            public void run() {
-                progressBar.setVisibility(finalProg_vis);
-                fab.setVisibility(finalFab_vis);
-                searchLayout.setVisibility(search_vis);
-            }
-        });
+
+        progressBar.setVisibility(finalProg_vis);
+        fab.setVisibility(finalFab_vis);
+        searchLayout.setVisibility(search_vis);
+
     }
 
-    private void updateListThread(RecyclerView mainPokemonRV, PokemonAdapter pokemonAdapter){
-        mainPokemonRV.post(new Runnable() {
-            @Override
-            public void run() {
-                pokemonAdapter.updateData();
-            }
-        });
+    private PokemonBrief parsePokemon(JSONObject obj){
+        PokemonBrief p = new PokemonBrief();
+        try{
+            p.setId(obj.getInt("id"));
+            p.setName(obj.getString("name"));
+            p.setIcon(obj.getString("icon"));
+            p.setLink(obj.getString("link"));
+            return p;
+        }
+        catch (JSONException e){
+            return null;
+        }
     }
 
 
